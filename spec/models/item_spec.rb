83fc17934a997ce849702f7e2afb7001942e0185
ponkantaichou <banshee7577@gmail.com>
2020-10-23 09:21:02 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -114,6 +114,12 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Price is not a number")
         end
+
+        it '画像が入ってない時' do
+          @item.image = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Image can't be blank")
+        end
       end
     end
 end
