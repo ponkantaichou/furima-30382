566d7118b98dd06dac77c2bf9148264f2517e172
ponkantaichou <banshee7577@gmail.com>
2020-10-28 15:29:23 +0900

    修正８

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+  before_action :set_order, only: [:index]
 
   def index
-    @user_order = UserOrder.new
-    if current_user.id == @item.user_id
-      redirect_to root_path
-    elsif @item.order
-      redirect_to root_path
-    end
   end
 
   def create
@@ -30,6 +25,15 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def set_order
+    @user_order = UserOrder.new
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    elsif @item.order
+      redirect_to root_path
+    end
   end
 
   def pay_item
